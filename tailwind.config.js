1b2116309a234e80e316bdd5a5315d612d8f9eb7
Dayem Siddiqui <dayem.siddiqui@personio.de>
2025-07-01 19:49:17 +0100

    Implemented firekit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   darkMode: ['class'],
   content: [
     './inertia/pages/**/*.{ts,tsx}',
@@ -16,7 +16,24 @@ module.exports = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ['Instrument Sans', 'sans-serif'],
+      },
       colors: {
+        sand: {
+          1: 'var(--sand-1)',
+          2: 'var(--sand-2)',
+          3: 'var(--sand-3)',
+          4: 'var(--sand-4)',
+          5: 'var(--sand-5)',
+          6: 'var(--sand-6)',
+          7: 'var(--sand-7)',
+          8: 'var(--sand-8)',
+          9: 'var(--sand-9)',
+          10: 'var(--sand-10)',
+          11: 'var(--sand-11)',
+          12: 'var(--sand-12)',
+        },
         border: 'hsl(var(--border))',
         input: 'hsl(var(--input))',
         ring: 'hsl(var(--ring))',
